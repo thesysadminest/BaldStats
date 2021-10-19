1622178a3012218d2bf2b8b3ef2256c8608ebfdc
DEN <dvgv35@gmail.com>
2021-10-19 17:57:36 +0300

    JSON parser added

--- BaldStats/main.cpp
+++ BaldStats/main.cpp
@@ -7,13 +7,18 @@
 #include <wx/sstream.h>
 #include <wx/webrequest.h>
 
+#include <wx/thread.h> //SPOILER??
+
 class Frame : public wxFrame {
 public:
 	Frame() : wxFrame(nullptr, wxID_ANY, "Testing", wxDefaultPosition, wxSize(500, 500)) {
 		button->Bind(wxEVT_BUTTON, &Frame::test, this);
 		q1->SetHint("Hypixel API");
+		q1->SetValue("f57c9f4a-175b-430c-a261-d8c199abd927");
 		q2->SetHint("Nickname");
 
+		//text->SetEditable(false);
+		//q2->SetEditable(false);
 	}
 
 private:
@@ -22,12 +27,90 @@ private:
 	wxTextCtrl* text = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, { 50, 50 }, wxSize((400), (300)), wxTE_MULTILINE);
 	wxTextCtrl* q1 = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, { 100, 10 }, wxSize((80), (20)));
 	wxTextCtrl* q2 = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, { 200, 10 }, wxSize((80), (20)));
-	void test(wxCommandEvent& e) {
-		//wxLogInfo(q1->GetValue());
-		text->SetValue(wxEmptyString);
-		Request();
+	wxTextCtrl* command = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, { 50, 370 }, wxSize((400), (20)));
+
+	void ReadLine(wxCommandEvent& e) {
+		wxString line = command->GetValue();
+		command->SetValue(wxEmptyString);
+		if (line.size() < 40) return;
+		if (line.substr(11, 21) != "[Client thread/INFO]:") return;
+		wxLogInfo("yes");
 	}
-	void Request() {
+
+
+	void test(wxCommandEvent& e) {
+		wxString json = text->GetValue();
+		int player_pos = jsonGetPos(0, "player", json);
+		wxString displayname = jsonGetValue(jsonGetPos(player_pos, "displayname", json), json);
+		wxString uuid = jsonGetValue(jsonGetPos(player_pos, "uuid", json), json);
+		int achievements_pos = jsonGetPos(player_pos, "achievements", json);
+		wxString bwlevel = jsonGetValue(jsonGetPos(achievements_pos, "bedwars_level", json), json);
+		wxString finalk = jsonGetValue(jsonGetPos(jsonGetPos(jsonGetPos(player_pos, "stats", json), "Bedwars", json), "final_kills_bedwars", json), json);
+		wxLogMessage(displayname);
+		wxLogMessage(uuid);
+		wxLogMessage(bwlevel);
+		wxLogMessage(finalk);
+	}
+
+
+	// returns index of ":" in " "key":"value {} []" "
+	// in not exist, returns -1;
+	int jsonGetPos(int start, wxString key, wxString& json) {
+		int level = 0;
+		int index = start;
+		while (true) {
+			index += 2;
+			bool flag = true;
+			for (unsigned int i2 = 0; i2 < key.size(); ++i2) {
+				if (json[index] != key[i2] || json[index] == '"') { flag = false; break; }
+				++index;
+			}
+			if (flag && json[index] == '"') return ++index;
+			if (json[index] != '"') while (json[index] != '"') ++index;
+			index += 2;
+
+			if (json[index] == '[') { while (json[index] != ']') ++index; ++index; }
+			else if (json[index] == '"') {
+				++index;
+				while (json[index] != '"') ++index;
+				++index;
+			}
+			else if (json[index] == '{') {
+				++level;
+				while (level != 0) {
+					++index;
+					if (json[index] == '{') ++level;
+					else if (json[index] == '}') --level;
+				}
+				++index;
+			}
+			else while (json[index] != ',') ++index;
+			if (json[index] == '}') return -1;
+		}
+		return -1;
+	}
+
+
+	// returns value after ":" index in " "key":"value" "
+	wxString jsonGetValue(int index, wxString& json) {
+		wxString res = wxEmptyString;
+		++index;
+		if (json[index] == '"') {
+			++index;
+			while (json[index] != '"') {
+				res.append(json[index]);
+				++index;
+			}
+		}
+		else while (json[index] != ',') while (json[index] != ',') {
+			res.append(json[index]);
+			++index;
+		}
+		return res;
+	}
+
+
+	void Request(wxCommandEvent& e) {
 		wxWebRequest request = wxWebSession::GetDefault().CreateRequest(
 			this,
 			"https://api.hypixel.net/player?key=" + q1->GetValue() + "&name=" + q2->GetValue()
@@ -57,9 +140,8 @@ private:
 		// Start the request
 		request.Start();
 	}
-
-
 };
+
 
 class Program : public wxApp {
 	bool OnInit() override {
