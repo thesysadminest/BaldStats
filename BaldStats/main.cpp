37cd6ec20abfdc1ef8db01ff6c81906ba787c729
DEN <dvgv35@gmail.com>
2021-10-26 00:30:01 +0300

    log line analyzer added

--- BaldStats/main.cpp
+++ BaldStats/main.cpp
@@ -3,19 +3,34 @@
 #include <wx/panel.h>
 #include <wx/button.h>
 #include <wx/textctrl.h>
+#include <wx/menu.h>
+#include <wx/msgdlg.h>
 
 #include <wx/sstream.h>
 #include <wx/webrequest.h>
 
 #include <wx/thread.h> //SPOILER??
 
+#include <vector>
+
 class Frame : public wxFrame {
 public:
 	Frame() : wxFrame(nullptr, wxID_ANY, "Testing", wxDefaultPosition, wxSize(500, 500)) {
-		button->Bind(wxEVT_BUTTON, &Frame::test, this);
+		button->Bind(wxEVT_BUTTON, &Frame::ReadLine, this);
 		q1->SetHint("Hypixel API");
 		q1->SetValue("f57c9f4a-175b-430c-a261-d8c199abd927");
 		q2->SetHint("Nickname");
+		wxMessageDialog(this, "Кто бы ты ни был, пожалуйста, закрой программу. Её использование в данный момент ни к чему не приведёт", "Внимание!", wxOK | wxCENTRE | wxICON_ERROR).ShowModal();
+
+		wxMenu* menuHelp = new wxMenu;
+		menuHelp->AppendRadioItem(-1, "elem");
+		menuHelp->AppendRadioItem(6, "ent");
+		menuHelp->Enable(6, false);
+		wxMenuBar* menuBar = new wxMenuBar;
+		menuBar->Append(menuHelp, "&Help");
+		SetMenuBar(menuBar);
+		//CreateStatusBar();
+		//SetStatusText("Welcome to wxWidgets!");
 
 		//text->SetEditable(false);
 		//q2->SetEditable(false);
@@ -29,32 +44,100 @@ private:
 	wxTextCtrl* q2 = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, { 200, 10 }, wxSize((80), (20)));
 	wxTextCtrl* command = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, { 50, 370 }, wxSize((400), (20)));
 
+
 	void ReadLine(wxCommandEvent& e) {
 		wxString line = command->GetValue();
 		command->SetValue(wxEmptyString);
 		if (line.size() < 40) return;
-		if (line.substr(11, 21) != "[Client thread/INFO]:") return;
-		wxLogInfo("yes");
+		if (line.substr(11, 28) != "[Client thread/INFO]: [CHAT]") return;
+		if (line[40] == '§') return; // if current line is message in the chat 
+
+		std::vector<wxString> parsed;
+		wxString temp = wxEmptyString;
+		for (unsigned int i = 40; i < line.size(); ++i) {
+			if (line[i] == ' ') {
+				if (temp != "[VIP]") parsed.push_back(temp);
+				temp = wxEmptyString;
+			}
+			else temp.append(line[i]);
+		}
+		if (!temp.empty()) parsed.push_back(temp);
+		//for (auto i : parsed) wxLogInfo(i);
+
+		if (parsed.size() == 3) {
+			// the player quits the lobby
+			if (parsed[1] == "has" && parsed[2] == "quit") {
+				wxLogInfo("the player quits the lobby");
+			}
+		}
+		else if (parsed.size() == 4) {
+			// player joins the party
+			if (parsed[1] == "joined" && parsed[3] == "party") {
+				wxLogInfo("player joins the party");
+			}
+			// you leave the party
+			else if (parsed[0] == "You" && parsed[1] == "left") {
+				wxLogInfo("you leave the party");
+			}
+			// player joins the lobby
+			else if (parsed[1] == "has" && parsed[2] == "joined") {
+				wxLogInfo("player joins the lobby");
+			}
+		}
+		else if (parsed.size() == 5) {
+			// player leaves the party
+			if (parsed[2] == "left" && parsed[4] == "party") {
+				wxLogInfo("player leaves the party");
+			}
+			// you join someone else's party
+			else if (parsed[0] == "You" && parsed[4] == "party!") {
+				wxLogInfo("you join someone else's party");
+			}
+		}
+		else if (parsed.size() == 7) {
+			// player gets removed from the party
+			if (parsed[1] == "has" && parsed[3] == "removed") {
+				wxLogInfo("player gets removed from the party");
+			}
+		}
+		else if (parsed.size() == 9 || parsed.size() == 12) {
+			// the party gets disbanded
+			if (parsed[1] == "party" && parsed[3] == "disbanded") {
+				wxLogInfo("the party gets disbanded");
+			}
+		}
+		// lobby players list
+		else if (parsed[0] == "ONLINE:") {
+			wxLogInfo("lobby players list");
+		}
+		// party players list
+		else if (parsed[2] == "partying") {
+			wxLogInfo("party players list");
+		}
+		return;
 	}
 
 
-	void test(wxCommandEvent& e) {
+	void getPlayerStats(wxCommandEvent& e) {
 		wxString json = text->GetValue();
 		int player_pos = jsonGetPos(0, "player", json);
 		wxString displayname = jsonGetValue(jsonGetPos(player_pos, "displayname", json), json);
 		wxString uuid = jsonGetValue(jsonGetPos(player_pos, "uuid", json), json);
 		int achievements_pos = jsonGetPos(player_pos, "achievements", json);
 		wxString bwlevel = jsonGetValue(jsonGetPos(achievements_pos, "bedwars_level", json), json);
-		wxString finalk = jsonGetValue(jsonGetPos(jsonGetPos(jsonGetPos(player_pos, "stats", json), "Bedwars", json), "final_kills_bedwars", json), json);
+		int bedwars_pos = jsonGetPos(jsonGetPos(player_pos, "stats", json), "Bedwars", json);
+		wxString finalk = jsonGetValue(jsonGetPos(bedwars_pos, "final_kills_bedwars", json), json);
+		wxString finald = jsonGetValue(jsonGetPos(bedwars_pos, "final_deaths_bedwars", json), json);
 		wxLogMessage(displayname);
 		wxLogMessage(uuid);
 		wxLogMessage(bwlevel);
 		wxLogMessage(finalk);
+		wxLogMessage(finald);
 	}
 
 
 	// returns index of ":" in " "key":"value {} []" "
-	// in not exist, returns -1;
+	// if not exist, returns -1 or runtime error;
 	int jsonGetPos(int start, wxString key, wxString& json) {
 		int level = 0;
 		int index = start;
@@ -93,6 +176,7 @@ private:
 
 	// returns value after ":" index in " "key":"value" "
 	wxString jsonGetValue(int index, wxString& json) {
+		if (index == -1) return wxEmptyString;
 		wxString res = wxEmptyString;
 		++index;
 		if (json[index] == '"') {
